a81ca5f509dfa582970fdc6b7e3ed1b6f605c391
Bugarin, Valeria <Valeria.Bugarin@haufe.com>
2020-11-01 20:11:01 +0100

    Implementing auth with firebase

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.js
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import SignIn from '../../components/sign-in/sign-in.component';
+import React from 'react'
+import SignIn from '../../components/sign-in/sign-in.component'
+import SignUp from '../../components/sign-up/sign-up.component'
 
-import './sign-in-and-sign-up.styles.scss';
+import './sign-in-and-sign-up.styles.scss'
 
 const SingInAndSignUpPage = () => {
-	return <div>
-		<SignIn />
-	</div>
+  return (
+    <div className='sign-in-and-sign-up'>
+      <SignIn />
+      <SignUp />
+    </div>
+  )
 }
 
-
-export default SingInAndSignUpPage;
+export default SingInAndSignUpPage
