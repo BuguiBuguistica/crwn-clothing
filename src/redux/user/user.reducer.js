e916f8ec7b58f3446cbcbff89f907a151cf87fe8
Bugarin, Valeria <Valeria.Bugarin@haufe.com>
2020-12-06 19:44:56 +0100

    redirect when user is logged in

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,5 +1,6 @@
 // reducer is a function, receives the state and the action
 // the action has a type (string) and a payload
+import {UserActionTypes} from './user.types';
 
 const INITIAL_STATE = {
   currentUser: null
@@ -7,11 +8,10 @@ const INITIAL_STATE = {
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case 'SET_CURRENT_USER':
-		console.log('reducer ', action.payload)
+    case UserActionTypes.SET_CURRENT_USER:
       return {
         ...state,
-        currentUser: action.payload
+        currentUser: action.payload && action.payload.currentUser
       }
     default:
       return state
